5f4c328b629cec02299c6da2c9e73abf28564aa7
sesour <sesour>
2020-08-30 12:12:38 +0900

    1. 윈도우앱 내장 브라우저에서 동작하는 배너 전환으로 수정 2. 슬라이딩 배너 소스 추가

--- sesour_music_player/js/slider.js
+++ sesour_music_player/js/slider.js
@@ -1,18 +1,41 @@
-var slideIndex = 0;
-showSlides();
+var slide_list;
+var slide_list_count;
+var slide_current_position = 0;
+var interval = 2000;
 
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+$(document).ready(function(){	
+	setTimeout(slide_next, interval);
+});
+
+function slide_next(){
+	var i;
+    var slides = $(".slide-container div");
     for (i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";  
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+    ++slide_current_position;
+    if (slide_current_position > slides.length){
+		{slide_current_position = 1}
+	}
+    slides[slide_current_position-1].style.display = "block";
+    setTimeout(slide_next, interval);
+}
+
+function slide_next_animation_sliding() {
+	slide_list = $(".sliding-container ul");
+	slide_list_count = slide_list.children().length;
+	
+	if(slide_list_count>(slide_current_position+1)){
+		slide_list.animate({
+			left:'-='+665*(slide_current_position+1)+'px'
+		});
+		++slide_current_position;
+	}else{
+		slide_list.animate({
+			left:'0px'
+		});
+		slide_current_position = 0;
+	}
+	
+	setTimeout(slide_next_animation_sliding, interval);	
 }
